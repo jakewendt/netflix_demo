9ce454554abbd03800dafdd0a5fb41cacd08045f
Jake <github@jakewendt.com>
2011-10-09 11:34:14 -0700

    Included rails and gems to make production host happy.

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
-#RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -14,6 +14,7 @@ Rails::Initializer.run do |config|
 	config.gem "xml-simple", :lib => 'xmlsimple'
 	config.gem "oauth"
 	config.gem "ruby-hmac", :lib => "hmac-sha1"
+	config.gem "rack", :version => "=1.1.2"
 
   config.time_zone = 'UTC'
 
